c9af08a623b33f7679b48b440b5e713edc7a0c0a
Marci <mezeimarton2000@gmail.com>
2021-08-31 19:13:51 +0200

    separate php codes

---
+++ get_datas.php
@@ -0,0 +1,18 @@
+<?php
+    include("config.php");
+    $query = "select hum, temp, date from TempHum order by date desc limit 1440;";
+    $ans = mysqli_query($db, $query);
+    $row = $ans->fetch_assoc();
+
+    $Currhum = $row["hum"];
+    $Currtemp = $row["temp"];
+    $Currdate = $row["date"];
+
+    $array = array(array($Currtemp, $Currhum, $date));
+
+    while($row = $ans->fetch_assoc())
+    {
+        array_push($array, array($row["temp"], $row["hum"], $row["date"]));
+    }
+    $array_res = array_reverse($array);    
+?>
--- index.php
+++ index.php
@@ -7,21 +7,7 @@
 </head>
 <?php
     include("config.php");
-    $query = "select hum, temp, date from TempHum order by date desc limit 1440;";
-    $ans = mysqli_query($db, $query);
-    $row = $ans->fetch_assoc();
-
-    $Currhum = $row["hum"];
-    $Currtemp = $row["temp"];
-    $Currdate = $row["date"];
-
-    $array = array(array($Currtemp, $Currhum, $date));
-
-    while($row = $ans->fetch_assoc())
-    {
-        array_push($array, array($row["temp"], $row["hum"], $row["date"]));
-    }
-    $array_res = array_reverse($array);    
+    include("get_datas.php");
 ?>
 
 <body stlye="width: 100%">
