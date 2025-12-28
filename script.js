332f84c7c795725e3008d663a4d45815c95bfa02
Marci <mezeimarton2000@gmail.com>
2025-12-28 22:18:42 +0100

    [improvement] - Highlight data gaps on plots

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var xAxis = new Array();
 var temps = new Array();
 var hums = new Array();
 
+let shapes = [];
+
 for(var i = 0; i < datas.length; i++){
     xAxis.push(datas[i][2]);
     temps.push(datas[i][0]);
@@ -87,6 +89,8 @@ var layoutHum = {
 };
 
 function update(interval){
+    shapes = [];
+
     let xAxis_needed = new Array();
     let temps_needed = new Array();  
     let hums_needed  = new Array();
@@ -105,6 +109,30 @@ function update(interval){
     });
 
     for(let i = closestIndex; i < datas.length; i++){
+        if (i > closestIndex) {
+            const currentTime = new Date(xAxis[i]).getTime();
+            const prevTime = new Date(xAxis[i - 1]).getTime();
+            const diffInMinutes = (currentTime - prevTime) / (1000 * 60);
+
+            if (diffInMinutes >= 6) {
+                xAxis_needed.push(xAxis[i - 1]);
+                temps_needed.push(null);
+                hums_needed.push(null);
+
+                shapes.push({
+                    type: 'rect',
+                    xref: 'x',
+                    yref: 'paper',
+                    x0: xAxis[i - 1],
+                    x1: xAxis[i],
+                    y0: 0,
+                    y1: 1,
+                    fillcolor: '#9e9e9eff',
+                    opacity: 0.5,
+                    line: { width: 0 }
+                });
+            }
+        }
         xAxis_needed.push(xAxis[i]);
         temps_needed.push(temps[i]);
         hums_needed.push(hums[i]);
@@ -141,6 +169,9 @@ function update(interval){
 
     let dataTemp = [traceTemp];
     let dataHum  = [traceHum];
+
+    layoutTemp.shapes = shapes;
+    layoutHum.shapes = shapes;
 
     Plotly.react('aTemp1', dataTemp, layoutTemp);
     Plotly.react('aHum1', dataHum, layoutHum);
